b39a16566c53cb46308c5c5cb7dd87d80d347705
sachin ghatole <ghatolesachin89@gmail.com>
2024-05-22 17:31:41 +0530

    first_update

---
+++ js/foot-loc.js
@@ -0,0 +1,14 @@
+﻿function toggle(){var e=document.getElementById("toggleText"),t=document.getElementById("displayText");"block"==e.style.display?(e.style.display="none",t.innerHTML="More Location"):(e.style.display="block",t.innerHTML="Less Location")}function PopUp(){document.getElementById("ac-wrapper").style.display="none"}
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
